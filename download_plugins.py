b032274d82ef7f65121fc90aeb09afb326681ff6
browni <browni@oregonstate.edu>
2018-06-28 09:31:27 -0700

    Update to python 3, adhere to pep 8

--- download_plugins.py
+++ download_plugins.py
@@ -1,47 +1,57 @@
-import requests, yaml, sys
+import requests
+import yaml
+import sys
 from distutils.version import LooseVersion
+
 
 # Get dependencies for a plugin, or get depedencies for a dependency.
 def get_dependencies(plugin):
-    print "Finding dependencies for:", plugin
-    print "*****************************************"
-    print plugin, "has these dependencies:"
+    print("Finding dependencies for:", plugin)
+    print("*****************************************")
+    print(plugin, "has these dependencies:")
 
     try:
         dependencies = plugins_list['plugins'][plugin]['dependencies']
     except KeyError:
-        print "Unable to find dependencies for %s." % plugin
+        print("Unable to find dependencies for %s." % plugin)
         global exit_code
         exit_code = 1
         return None
-    
+
     for dependency in dependencies:
-        print "Processing dependency: " + dependency['name']
+        print("Processing dependency: " + dependency['name'])
 
         if dependency['optional']:
-                print dependency['name'] + " is optional"
+                print(dependency['name'] + " is optional")
 
         elif (dependency['name'] in stored_plugins):
-            if (LooseVersion(dependency['version']) <= LooseVersion(stored_plugins[dependency['name']])):
-                print "Newer or same version of dependency already added"
+            if (LooseVersion(dependency['version']) <=
+                    LooseVersion(stored_plugins[dependency['name']])):
+                print("Newer or same version of dependency already added")
             else:
-                print "Adding new version of dependency"
+                print("Adding new version of dependency")
                 stored_plugins[dependency['name']] = dependency['version']
             if dependency['name'] not in multi_version_plugins:
                     multi_version_plugins.append(dependency['name'])
-                    
+
         else:
-            print "Adding dependency " + dependency['name']
+            print("Adding dependency " + dependency['name'])
             stored_plugins[dependency['name']] = dependency['version']
 
-# Download plugin from jenkins update server. If no version is specified, latest plugin is downloaded.
-def download_plugin(plugin, version = None):
+
+# Download plugin from jenkins update server.
+# If no version is specified, latest plugin is downloaded.
+def download_plugin(plugin, version=None):
     download_url = ""
-    
+
     if version is None:
         download_url = plugin_base_url + "/latest/%s.hpi" % plugin
     else:
-        download_url = plugin_base_url + "/download/plugins/%(plugin)s/%(version)s/%(plugin)s.hpi" % {'plugin': plugin, 'version': version}
+        download_url = (plugin_base_url +
+                        "/download/plugins/" +
+                        "{plugin}/{version}/{plugin}.hpi".format(
+                            plugin=plugin, version=version
+                        ))
 
     plugin_download = requests.get(download_url, stream=True)
 
@@ -52,30 +62,32 @@ def download_plugin(plugin, version = None):
             for chunk in plugin_download.iter_content(chunk_size=128):
                 data.write(chunk)
 
-        print "Downloaded", plugin
-        
+        print("Downloaded", plugin)
+
     else:
-        print "Error downloading %s. Response:" % plugin
-        print plugin_download.text
+        print("Error downloading %s. Response:" % plugin)
+        print(plugin_download.text)
         global exit_code
         exit_code = 1
+
 
 # Install each plugin in the supplied json file along with dependencies.
 def install_plugins():
     for plugin, version in plugins.items():
-        print "**** Install Plugin: %s ****" % plugin
+        print("**** Install Plugin: %s ****" % plugin)
         download_plugin(plugin, version)
         get_dependencies(plugin)
 
-    print "Installing all dependencies"
-    print "*****************************************"
+    print("Installing all dependencies")
+    print("*****************************************")
     for plugin, version in stored_plugins.items():
         download_plugin(plugin, version)
 
     if (multi_version_plugins):
         for plugin in multi_version_plugins:
-            print "Warning: Multiple versions of %s found in dependencies." % plugin
-            print "Version %s downloaded." % stored_plugins[plugin]
+            print("Warning: Multiple versions of " +
+                  " %s found in dependencies." % plugin)
+            print("Version %s downloaded." % stored_plugins[plugin])
 
 plugins_file_path = str(sys.argv[1])
 download_directory = str(sys.argv[2])
@@ -84,18 +96,19 @@ try:
     with open(plugins_file_path, 'r') as file:
         plugins = yaml.load(file)['plugins']
 except (IOError, ValueError):
-    print "Unable to load plugin yaml file"
+    print("Unable to load plugin yaml file")
     sys.exit(1)
 
 plugin_base_url = "https://updates.jenkins.io"
 stored_plugins = {}
 multi_version_plugins = []
 
-plugins_list_request = requests.get(plugin_base_url + "/current/update-center.actual.json")
+plugins_list_request = requests.get(plugin_base_url +
+                                    "/current/update-center.actual.json")
 
 if plugins_list_request.status_code is not 200:
-    print "Unable to get plugin data. Response:"
-    print plugins_list_request.text
+    print("Unable to get plugin data. Response:")
+    print(plugins_list_request.text)
     sys.exit(1)
 
 plugins_list = plugins_list_request.json()
