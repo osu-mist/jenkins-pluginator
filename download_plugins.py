472f71d1bc13f2425d23ebe3d5e404827a5a1717
browni <browni@oregonstate.edu>
2018-07-03 12:47:52 -0700

    Check versions of top-level plugins, additional warnings

--- download_plugins.py
+++ download_plugins.py
@@ -1,13 +1,15 @@
+import utils
 import requests
 import yaml
 import sys
+from logging import debug
 from textwrap import dedent
 from distutils.version import LooseVersion
 
 
 # Get dependencies for a plugin, or get depedencies for a dependency.
 def get_dependencies(plugin):
-    print(dedent("""
+    debug(dedent("""
     Finding dependencies for: {0}
     ******************************************
     {0} has these dependencies:""".format(plugin)))
@@ -15,41 +17,42 @@ def get_dependencies(plugin):
     try:
         dependencies = plugins_list["plugins"][plugin]["dependencies"]
     except KeyError:
-        print("Unable to find dependencies for {}.".format(plugin))
+        debug("Unable to find dependencies for {}.".format(plugin))
         global exit_code
         exit_code = 1
         return None
 
     for dependency in dependencies:
-        print("Processing dependency: {}".format(dependency["name"]))
+        dep_version = dependency["version"]
+        dep_name = dependency["name"]
+        debug("Processing dependency: {}".format(dep_name))
 
         if dependency["optional"]:
-            print("{} is optional".format(dependency["name"]))
+            debug("{} is optional".format(dep_name))
             continue
-
-        elif dependency["name"] in stored_plugins:
-            if (LooseVersion(dependency["version"]) <=
-                    LooseVersion(stored_plugins[dependency["name"]])):
-                print("Newer or same version of dependency already added")
-            else:
-                print("Adding new version of dependency")
-                stored_plugins[dependency["name"]] = dependency["version"]
-                get_dependencies(dependency["name"])
-            if not (any(p.get(dependency["version"], None) == plugin for p in
-                    dependencies_info[dependency["name"]]["parents"])):
-                version_sorted_insert(dependency, plugin)
-
         else:
-            print("Adding dependency: {}".format(dependency["name"]))
-            stored_plugins[dependency["name"]] = dependency["version"]
-            dependencies_info[dependency["name"]] = {
-                "duplicate": False,
-                "parents": []
-            }
-            dependencies_info[dependency["name"]]["parents"].append(
-                {dependency["version"]: plugin}
-            )
-            get_dependencies(dependency["name"])
+            add_dep(dep_name, dep_version, plugin)
+
+
+def add_dep(dep_name, dep_version, parent):
+    if dep_name in stored_plugins:
+        if (utils.not_newer(dep_version, stored_plugins[dep_name])):
+            debug("Newer or same version of dependency already added")
+        else:
+            debug("Adding new version of dependency")
+            stored_plugins[dep_name] = dep_version
+            get_dependencies(dep_name)
+        # Insert dependency entry if not already in dep_info
+        if not (any(p.get(dep_version, None) == parent for p in
+                dep_info[dep_name]["parents"])):
+            version_sorted_insert(dep_name, dep_version, parent)
+
+    else:
+        debug("Adding dependency: {}".format(dep_name))
+        stored_plugins[dep_name] = dep_version
+        dep_info[dep_name] = {"duplicate": False, "parents": []}
+        dep_info[dep_name]["parents"].append({dep_version: parent})
+        get_dependencies(dep_name)
 
 
 # Download plugin from jenkins update server.
@@ -57,16 +60,11 @@ def get_dependencies(plugin):
 def download_plugin(plugin, version=None):
     download_url = ""
 
-    if version is None:
-        download_url = "{url}/latest/{plugin}.hpi".format(
-            url=plugin_base_url, plugin=plugin
-        )
-    else:
-        download_url = (plugin_base_url +
-                        "/download/plugins/" +
-                        "{plugin}/{version}/{plugin}.hpi".format(
-                            plugin=plugin, version=version
-                        ))
+    download_url = ("{url}/download/plugins/"
+                    "{plugin}/{version}/{plugin}.hpi".format(
+                        url=plugin_base_url, plugin=plugin,
+                        version=version
+                    ))
 
     plugin_download = requests.get(download_url, stream=True)
 
@@ -78,8 +76,14 @@ def download_plugin(plugin, version=None):
         with open(destination_path, "wb") as data:
             for chunk in plugin_download.iter_content(chunk_size=128):
                 data.write(chunk)
-
-        print("Downloaded {}".format(plugin))
+        if plugin in plugins:
+            print("Downloaded {plugin}: {version} [TOP-LEVEL]".format(
+                plugin=plugin, version=stored_plugins[plugin]
+            ))
+        else:
+            print("Downloaded {plugin}: {version}".format(
+                plugin=plugin, version=stored_plugins[plugin]
+            ))
 
     else:
         print("Error downloading {plugin}. Response:\n{response}".format(
@@ -92,16 +96,18 @@ def download_plugin(plugin, version=None):
 # Install each plugin in the supplied json file along with dependencies.
 def install_plugins():
     for plugin, version in plugins.items():
-        print("\n**** Install Plugin: {} *****".format(plugin))
-        download_plugin(plugin, version)
+        debug("\n**** Add Plugin: {} *****".format(plugin))
+        if version is None:
+            version = plugins_list["plugins"][plugin]["version"]
+        add_dep(plugin, version, plugin)
         get_dependencies(plugin)
 
-    print("\nDownloading all dependencies")
-    print("*****************************************")
+    debug("\nDownloading all dependencies")
+    debug("*****************************************")
     for plugin, version in stored_plugins.items():
         download_plugin(plugin, version)
 
-    for dependency, info in dependencies_info.items():
+    for dependency, info in dep_info.items():
         if not (info["duplicate"]):
             continue
         print(dedent("""\
@@ -109,55 +115,64 @@ def install_plugins():
         """.format(dependency)))
         for parent in info["parents"]:
             for version, plugin in parent.items():
-                print("    Version {version} required by {plugin}".format(
-                    version=version, plugin=plugin
-                ))
+                if dependency == plugin:
+                    print("    !! TOP-LEVEL version: {} !!".format(version))
+                else:
+                    print("    Version {version} required by {plugin}".format(
+                        version=version, plugin=plugin
+                    ))
         print("    Downloaded version {}".format(stored_plugins[dependency]))
+
+    # Print warning for plugin if downloaded version != specified version
+    for plugin, version in plugins.items():
+        if version != stored_plugins[plugin]:
+            print("Warning: TOP-LEVEL version of {plugin} ({spec_ver}) "
+                  "not same as downloaded ({real_ver})".format(
+                    plugin=plugin, spec_ver=version,
+                    real_ver=stored_plugins[plugin]
+                    ))
 
 
 # Insert a new dependency parent sorted by version
-def version_sorted_insert(dependency, plugin):
-    for idx, elem in enumerate(
-            dependencies_info[dependency["name"]]["parents"]):
+def version_sorted_insert(dep_name, dep_version, plugin):
+    for idx, elem in enumerate(dep_info[dep_name]["parents"]):
         for version, parent in elem.items():
-            if (LooseVersion(dependency["version"]) <=
-                    LooseVersion(version)):
-                dependencies_info[dependency["name"]]["parents"].insert(
-                    idx, {dependency["version"]: plugin}
+            if(utils.not_newer(dep_version, version)):
+                dep_info[dep_name]["parents"].insert(
+                    idx, {dep_version: plugin}
                 )
-                if (LooseVersion(dependency["version"]) <
-                        LooseVersion(version)):
-                    dependencies_info[dependency["name"]]["duplicate"] = True
+                if utils.is_older(dep_version, version):
+                    dep_info[dep_name]["duplicate"] = True
                 return
-    dependencies_info[dependency["name"]]["parents"].append(
-        {dependency["version"]: plugin}
-    )
-    dependencies_info[dependency["name"]]["duplicate"] = True
+    dep_info[dep_name]["parents"].append({dep_version: plugin})
+    dep_info[dep_name]["duplicate"] = True
 
 
-plugins_file_path = str(sys.argv[1])
-download_directory = str(sys.argv[2])
+if __name__ == "__main__":
+    args = utils.parse_args()
+    plugins_file_path = args.jenkins_plugins_config
+    download_directory = args.download_dir
 
-try:
-    with open(plugins_file_path, "r") as file:
-        plugins = yaml.load(file)["plugins"]
-except (IOError, ValueError):
-    sys.exit("Unable to load plugin yaml file")
+    try:
+        with open(plugins_file_path, "r") as file:
+            plugins = yaml.load(file)["plugins"]
+    except (IOError, ValueError):
+        sys.exit("Unable to load plugin yaml file")
 
-plugin_base_url = "https://updates.jenkins.io"
-stored_plugins = {}
-dependencies_info = {}
+    plugin_base_url = "https://updates.jenkins.io"
+    stored_plugins = {}
+    dep_info = {}
 
-plugins_list_request = requests.get(plugin_base_url +
-                                    "/current/update-center.actual.json")
+    plugins_list_request = requests.get(plugin_base_url +
+                                        "/current/update-center.actual.json")
 
-if plugins_list_request.status_code != 200:
-    sys.exit("Unable to get plugin data. Response:\n{}".format(
-        plugins_list_request.text
-    ))
+    if plugins_list_request.status_code != 200:
+        sys.exit("Unable to get plugin data. Response:\n{}".format(
+            plugins_list_request.text
+        ))
 
-plugins_list = plugins_list_request.json()
-exit_code = 0
+    plugins_list = plugins_list_request.json()
+    exit_code = 0
 
-install_plugins()
-sys.exit(exit_code)
+    install_plugins()
+    sys.exit(exit_code)
